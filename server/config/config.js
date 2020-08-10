277b426d599bfc3974859bc480d0de4ba9f02c6f
rafaelvalentine <ozichukwuezike@hotmail.com>
2020-08-10 09:33:32 +0100

    deleted prod config

--- server/config/config.js
+++ server/config/config.js
@@ -7,19 +7,4 @@ if (env === 'development' || env === 'test') {
     Object.keys(envConfig).forEach(key => {
         process.env[key] = envConfig[key]
     })
-} else if (env === 'production') {
-    const config = require('./config.json')
-    const envConfig = config[env]
-    Object.keys(envConfig).forEach(key => {
-        process.env[key] = envConfig[key]
-    })
-} else {
-
 }
-// if (env === 'development') {
-//     process.env.PORT = 3000
-//     process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp'
-// } else if (env === 'test') {
-//     process.env.PORT = 3000
-//     process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest'
-// }
